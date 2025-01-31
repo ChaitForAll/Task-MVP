77c409ce37d5cce4886ef0ca25c58b6e248c58e5
yjjem <marksim0606@gmail.com>
2025-01-31 23:56:53 +0900

    Initial Project Setup

---
+++ TaskMVP/TaskMVP/AppEntry/SceneDelegate.swift
@@ -0,0 +1,30 @@
+//
+//  SceneDelegate.swift
+//  TaskMVP
+//
+//  Copyright (c) 2025 Jeremy All rights reserved.
+    
+
+import UIKit
+
+final class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    //MARK: Property(s)
+    
+    var window: UIWindow?
+
+    //MARK: Function(s)
+
+    func scene(
+        _ scene: UIScene, 
+        willConnectTo session: UISceneSession,
+        options connectionOptions: UIScene.ConnectionOptions
+    ) {
+        guard let windowScene = scene as? UIWindowScene else { return }
+        let window = UIWindow(windowScene: windowScene)
+        window.makeKeyAndVisible()
+        window.rootViewController = ViewController()
+        self.window = window
+    }
+}
+
---
+++ TaskMVP/TaskMVP/Scene/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  TaskMVP
+//
+//  Copyright (c) 2025 Jeremy All rights reserved.
+    
+
+import UIKit
+
+final class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemGreen
+    }
+
+}
+
